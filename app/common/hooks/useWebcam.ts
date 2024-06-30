db43d5177d7af314ca3ebef234913637c3676188
ilyadubov2004@gmail.com <ilyadubov2004@gmail.com>
2024-06-30 19:51:27 +0200

    test

--- app/common/hooks/useWebcam.ts
+++ app/common/hooks/useWebcam.ts
@@ -20,6 +20,7 @@ export const useWebcam = () => {
       });
       setMediaStream(response);
     } catch (error) {
+      console.log(error);
       setIsError(true);
     }
     setInitialized(true);
